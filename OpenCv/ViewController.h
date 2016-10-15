d3ada3e90cd768137cf1586748741393ad58fbad
Riches <Riches@RichesdeMac-mini.local>
2016-10-15 18:07:09 +0800

    Initial Commit

---
+++ OpenCv/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenCv
+//
+//  Created by Riches on 2016/10/15.
+//  Copyright © 2016年 Riches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
